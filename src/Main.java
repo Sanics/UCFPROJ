b7a3268e1f2b308c67e7038025db0dbf14138499
Rohan <Roxas240@gmail.com>
2016-02-24 10:01:10 -0500

    Added findFactors

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,38 @@ public class Main {
     public static final String file = "INPUT.TXT";
 
     public static void main(String[] args){
+        for(int n = 0; n < 100000; n++) {
+            int[] ints = getFactors(n);
+            System.out.print("Num: " + n + ", Factors: ");
+            for (int i : ints) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
 
+    public static boolean isPrime(int f){
+        return getFactors(f).length == 0;
+    }
+
+    public static int[] getFactors(int f){
+        ArrayList<Integer> a = new ArrayList<Integer>();
+
+        for(int i = 2; i < f; i++){
+            int ceil = (int)Math.ceil((float)f / (float)i);
+            int div = f / i;
+
+            if(ceil == div){
+                a.add(i);
+            }
+        }
+
+        int[] ret = new int[a.size()];
+        for(int i = 0; i < ret.length; i++){
+            ret[i] = a.get(i);
+        }
+
+        return ret;
     }
 
     public ArrayList<String> readFile(String fileName){
